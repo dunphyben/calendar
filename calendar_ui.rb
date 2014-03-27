a5f5e0610a712c1869535c6e3e20b1c68bb5867b
Josh Fowler & Benjamin Paul Dunphy <student@epicodus.com>
2014-03-27 12:32:17 -0700

    Add UI methods list-day, -week, -month events. Add scrolling option between days.

--- calendar_ui.rb
+++ calendar_ui.rb
@@ -14,15 +14,17 @@ end
 def main_menu
   choice = nil
   until choice == 'X'
+    puts "*"*40, "MAIN MENU", "*"*40
     puts "\tC - Create an event.",
-         "\tL - List All Events",
+         "\tE - Existing Events",
          "\tX - Exit"
     choice = gets.chomp.upcase
     case choice
     when 'C'
       create_event
-    when 'L'
-      list_events
+      gets
+    when 'E'
+      existing_events
     when 'X'
     else
       puts "Invalid input"
@@ -31,6 +33,36 @@ def main_menu
 end
 
 # Next, let users list out the events in the order in which they will occur. By default, only list events in the future.
+
+
+def existing_events
+  choice = nil
+  until choice == 'X'
+    puts "*"*40, "Existing Events\n", "*"*40
+    puts "\tL: List all future events",
+         "\tT: List all events for today",
+         "\tW: List all events for this week",
+         "\tM: List all events for this month",
+         "\tX: Return to main menu"
+
+    choice = gets.chomp.upcase
+    case choice
+    when 'L'
+      list_future_events
+      gets
+    when 'T'
+      list_todays_events
+      gets
+    when 'W'
+      list_weeks_events
+      gets
+    when 'M'
+      list_months_events
+      gets
+    when 'X'
+    end
+  end
+end
 
 
 def create_event
@@ -60,22 +92,85 @@ def create_event
   end_hour = gets.chomp.to_i
   end_time = DateTime.new(end_year, end_month, end_day, end_hour, 0, 0)
   new_event = Event.create(desc: description, location: location, start_time: start_time, end_time: end_time)
-  puts "Your event '#{description}' has been created!\n\n"
+  puts "\n\nYour event '#{description}' has been created!\n\n"
+  puts "Press Enter to continue."
 
 end
 
-def list_events
-  puts "LIST OF EVENTS\n\n"
+def list_future_events
+  puts "\n\n", "^"*25, "LIST OF EVENTS", "-"*35, "\n\n"
   time = Time.now
 
-
-  event_list = Event.all.each.sort_by { |event| event.start_time }
-
-  event_list.each do |event|
+  Event.all.order("start_time").each do |event|
     if event.end_time > time
-      puts "Event Name: " + event.desc, "Location: " + event.location, "Start Time: " + event.start_time.to_s, "End Time: " + event.end_time.to_s, "-"*40
+      puts "\n\nEvent Name: " + event.desc, "Location: " + event.location, "Start Time: " + event.start_time.to_s, "End Time: " + event.end_time.to_s, "\n\n", "-"*40
     end
   end
+  puts "Press Enter to continue."
+
 end
+
+
+
+def list_todays_events
+
+  puts "\n\n", "*"*40, "Today's Events", "_"*30
+  # event_list = Event.all.each.sort_by { |event| event.start_time }
+  counter = 0
+  loop do
+    Event.all.order("start_time").each do |event|
+      if event.start_time.mday == Time.now.mday+counter && event.start_time.year == Time.now.year+counter
+        puts "\n\nEvent Name: " + event.desc, "Location: " + event.location, "Start Time: " + event.start_time.to_s, "End Time: " + event.end_time.to_s, "\n\n", "-"*40
+      end
+    end
+    puts "N - Next day's events",
+         "P - Previous day's events",
+         "X - Back to menu"
+    choice = gets.chomp.upcase
+    case choice
+    when "N"
+      counter += 1
+    when "P"
+      counter -= 1
+    when "X"
+      break
+    else
+      puts "Invalid input"
+    end
+
+    puts "Press Enter to continue."
+  end
+end
+
+
+
+
+
+def list_weeks_events
+  puts "\n\n", "*"*40, "This Week's Events", "_"*30
+  # event_list = Event.all.each.sort_by { |event| event.start_time }
+
+  Event.all.order("start_time").each do |event|
+    if event.start_time.to_date.cweek == Time.now.to_date.cweek && event.start_time.year == Time.now.year
+      puts "\n\nEvent Name: " + event.desc, "Location: " + event.location, "Start Time: " + event.start_time.to_s, "End Time: " + event.end_time.to_s, "\n\n", "-"*40
+    end
+  end
+  puts "Press Enter to continue."
+end
+
+def list_months_events
+  puts "\n\n", "*"*40, "This Month's Events", "_"*30
+  # event_list = Event.all.each.sort_by { |event| event.start_time }
+
+  Event.all.order("start_time").each do |event|
+    if event.start_time.mon == Time.now.mon && event.start_time.year == Time.now.year
+      puts "\n\nEvent Name: " + event.desc, "Location: " + event.location, "Start Time: " + event.start_time.to_s, "End Time: " + event.end_time.to_s, "\n\n", "-"*40
+    end
+  end
+  puts "Press Enter to continue."
+end
+# Now, create a view that lets users choose to only view events on the current day, week, or month.
+
+
 
 welcome
